0b5fa59352e7d5cd993d735f3c272df656d95bd9
Duy Hai <duyhai121120@gmail.com>
2024-06-19 16:06:27 +0900

    Add java-basic

---
+++ src/HelloJava.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] arg) {
+        // 출력
+        System.out.println("Hello Java!!");
+
+        //
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+
+        // 해당 내용 각 주석으로
+    }
+}
